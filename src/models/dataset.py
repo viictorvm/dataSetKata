af63ed0b8d01396c81de86066ccfb625bef4fac6
victor <viictorvallecillo@gmail.com>
2019-07-08 12:32:29 +0200

    Update date time value. Add requirements needed

--- src/models/dataset.py
+++ src/models/dataset.py
@@ -1,13 +1,15 @@
-from sqlalchemy import Column, FLOAT, Integer, String
+from sqlalchemy import Column, FLOAT, Integer, String, Date
 from sqlalchemy.ext.declarative import declarative_base
 
 DeclarativeBase = declarative_base()
 
 
 class DataSet(DeclarativeBase):
+    DATE_TIME_FORMAT = '%d-%m-%Y'
+
     __tablename__ = 'dataset'
     id = Column(Integer, primary_key=True, autoincrement=True)
-    date = Column(String)
+    date_to = Column(Date)
     channel = Column(String)
     country = Column(String)
     os = Column(String)
@@ -19,7 +21,7 @@ class DataSet(DeclarativeBase):
 
     def to_json(self):
         return {
-            "date": self.date,
+            "date": self.date_to,
             "channel": self.channel,
             "country": self.country,
             "os": self.os,
